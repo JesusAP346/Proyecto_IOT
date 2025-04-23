b5c25bbe38e28dbcfc7eb780246947fddb8a10df
Jesus Alvarado <a20211688@pucp.edu.pe>
2025-04-22 19:38:11 -0500

    plantilla

---
+++ app/src/main/java/com/example/proyecto_iot/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.proyecto_iot;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.proyecto_iot.databinding.ActivityMainBinding;
+import com.example.proyecto_iot.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        replaceFragment(new PerfilFragment());
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.perfil) {
+                replaceFragment(new PerfilFragment());
+            } else if (itemId == R.id.qr) {
+                replaceFragment(new QrFragment());
+            } else if (itemId == R.id.solicitudes) {
+                replaceFragment(new SolicitudesFragment());
+            }
+
+            return true;
+        });
+
+        /*  NO SE QUE HACE ESTO PERO NO PARECE NECESARIO
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        */
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+
+
+
+}
