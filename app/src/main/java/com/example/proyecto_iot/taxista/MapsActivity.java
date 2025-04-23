cc106ab1ee9362e4a476c5b2dd0a8bef201b7677
Jesus Alvarado <a20211688@pucp.edu.pe>
2025-04-23 13:16:54 -0500

    vista maps

--- app/src/main/java/com/example/proyecto_iot/taxista/MapsActivity.java
+++ app/src/main/java/com/example/proyecto_iot/taxista/MapsActivity.java
@@ -1,6 +1,8 @@
 package com.example.proyecto_iot.taxista;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +19,16 @@ public class MapsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_maps);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        ImageView btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); //  Esto cierra la actividad y regresa a la anterior
+            }
         });
+
+
     }
 }
