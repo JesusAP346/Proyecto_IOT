6fc9e8cba8568c40b62d70b1b8667f03f1c61436
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2025-04-27 22:18:25 -0500

    Registro nuevo usuario parte 1

--- app/src/main/java/com/example/proyecto_iot/login/LoginActivity.java
+++ app/src/main/java/com/example/proyecto_iot/login/LoginActivity.java
@@ -1,12 +1,16 @@
-package com.example.proyecto_iot;
+package com.example.proyecto_iot.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.proyecto_iot.R;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +23,11 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TextView textCrearCuenta = findViewById(R.id.registro_nuevo_usuario);
+        textCrearCuenta.setOnClickListener(v ->{
+            Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
+            startActivity(intent);
         });
     }
 }
---
+++ app/src/main/java/com/example/proyecto_iot/login/RegistroActivity.java
@@ -0,0 +1,36 @@
+package com.example.proyecto_iot.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.proyecto_iot.R;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView textYaTengoCuenta = findViewById(R.id.ya_tengo_cuenta_txt);
+
+        textYaTengoCuenta.setOnClickListener(v ->{
+            Intent intent = new Intent(RegistroActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
