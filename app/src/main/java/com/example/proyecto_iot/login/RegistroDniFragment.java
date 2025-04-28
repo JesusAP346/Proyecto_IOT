10994d85df62d378c3ffaaf76d7d0a71622d3ee2
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2025-04-28 10:26:20 -0500

    Registro parte 3

--- app/src/main/java/com/example/proyecto_iot/login/RegistroDniFragment.java
+++ app/src/main/java/com/example/proyecto_iot/login/RegistroDniFragment.java
@@ -3,21 +3,19 @@ package com.example.proyecto_iot.login;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.proyecto_iot.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
+ * Use the {@link RegistroDniFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RegisterFragment extends Fragment {
+public class RegistroDniFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +26,7 @@ public class RegisterFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public RegisterFragment() {
+    public RegistroDniFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +36,11 @@ public class RegisterFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
+     * @return A new instance of fragment RegistroDniFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
+    public static RegistroDniFragment newInstance(String param1, String param2) {
+        RegistroDniFragment fragment = new RegistroDniFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,20 +60,7 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
-        Button botonSiguiente = view.findViewById(R.id.botonSiguiente);
-        botonSiguiente.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                RegistroDniFragment registroDniFragment = new RegistroDniFragment();
-                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragment_container, registroDniFragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_registro_dni, container, false);
     }
 }
