5ae5e3098eff09bec68ac01090609866cf9489db
RicardoEGFST <147290010+RicardoEGFST@users.noreply.github.com>
2025-05-01 13:58:17 -0500

    menu_superadmin

---
+++ app/src/main/java/com/example/proyecto_iot/SuperAdmin/PagPrincipalSuperAdmin.java
@@ -0,0 +1,57 @@
+package com.example.proyecto_iot.SuperAdmin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.SuperAdmin.fragmentos.fragment_home_superadmin;
+import com.example.proyecto_iot.SuperAdmin.fragmentos.fragment_administradores_superadmin;
+import com.example.proyecto_iot.SuperAdmin.fragmentos.fragment_reportes_superadmin;
+import com.example.proyecto_iot.SuperAdmin.fragmentos.fragment_usuarios_superadmin;
+import com.example.proyecto_iot.SuperAdmin.fragmentos.fragment_taxistas_superadmin;
+import com.example.proyecto_iot.SuperAdmin.fragmentos.fragment_reservas_superadmin;
+import com.example.proyecto_iot.databinding.ActivityMainSuperAdminBinding;
+
+public class PagPrincipalSuperAdmin extends AppCompatActivity {
+
+    ActivityMainSuperAdminBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainSuperAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Fragmento por defecto
+        replaceFragment(new fragment_home_superadmin());
+
+        binding.menuNavSup.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                replaceFragment(new fragment_home_superadmin());
+            } else if (itemId == R.id.nav_admins) {
+                replaceFragment(new fragment_administradores_superadmin());
+            } else if (itemId == R.id.nav_taxis) {
+                replaceFragment(new fragment_taxistas_superadmin());
+            } else if (itemId == R.id.nav_users) {
+                replaceFragment(new fragment_usuarios_superadmin());
+            } else if (itemId == R.id.nav_reservas) {
+                replaceFragment(new fragment_reservas_superadmin());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.commit();
+    }
+}
