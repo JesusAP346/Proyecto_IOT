17c5d57408cdcf62e56b8fdb54de48ff7d0599f8
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2025-05-01 00:13:34 -0500

    Uwu2222

--- app/src/main/java/com/example/proyecto_iot/cliente/busqueda/ClienteBusquedaActivity.java
+++ app/src/main/java/com/example/proyecto_iot/cliente/busqueda/ClienteBusquedaActivity.java
@@ -22,5 +22,10 @@ public class ClienteBusquedaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container_busqueda, new BusquedaFragment())
+                    .commit();
+        }
     }
 }
