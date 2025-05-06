a0d79364958ac0bdb7716cb150cf5f60aa874634
Jesus Alvarado <a20211688@pucp.edu.pe>
2025-05-06 16:35:40 -0500

    idea cambiar foto

--- app/src/main/java/com/example/proyecto_iot/taxista/perfil/PerfilTaxistaActivity.java
+++ app/src/main/java/com/example/proyecto_iot/taxista/perfil/PerfilTaxistaActivity.java
@@ -1,15 +1,24 @@
 package com.example.proyecto_iot.taxista.perfil;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.proyecto_iot.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class PerfilTaxistaActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +26,52 @@ public class PerfilTaxistaActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_perfil_taxista);
 
-
         ImageView btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish(); //  Esto cierra la actividad y regresa a la anterior
-            }
+        btnBack.setOnClickListener(v -> finish());
+
+        TextView btnEditar = findViewById(R.id.btnEditar);
+
+        btnEditar.setOnClickListener(v -> {
+            BottomSheetDialog dialog = new BottomSheetDialog(PerfilTaxistaActivity.this);
+            View sheetView = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_taxista, null);
+
+            //ImageView btnCerrar = sheetView.findViewById(R.id.btnCerrar);
+            TextView btnCambiar = sheetView.findViewById(R.id.btnCambiarFoto);
+            imageView = sheetView.findViewById(R.id.ivFoto);
+
+            Button btnListo = sheetView.findViewById(R.id.btnListo);
+
+            /*btnCerrar.setOnClickListener(e -> {
+                dialog.dismiss();
+            });*/
+
+            btnCambiar.setOnClickListener(e -> {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 100);  // 100 es un requestCode arbitrario
+            });
+
+
+            btnListo.setOnClickListener(e -> {
+                dialog.dismiss();
+                Toast.makeText(this, "Cambios guardados", Toast.LENGTH_SHORT).show();
+                // Aquí podrías guardar cambios si fuese necesario
+            });
+
+            dialog.setContentView(sheetView);
+            dialog.show();
         });
-
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            // Aquí puedes mostrarla en un ImageView por ejemplo:
+            imageView.setImageURI(selectedImageUri);
+        }
+    }
+
 }
