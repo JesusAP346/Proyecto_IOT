0ba01b558a50894671a00331f61fe3a972a535b4
brandon1502 <brandon16in.m@gmail.com>
2025-05-08 01:15:15 -0500

    falta arreglar el retroceso del recyclerview de habitaciones

---
+++ app/src/main/java/com/example/proyecto_iot/administradorHotel/entity/Servicio.java
@@ -0,0 +1,42 @@
+package com.example.proyecto_iot.administradorHotel.entity;
+
+import java.util.List;
+
+public class Servicio {
+
+    private String nombre;
+    private String descripcion;
+    private int precio;
+
+    //FALTA FOTOS
+
+    public Servicio(String nombre, String descripcion, int precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+}
