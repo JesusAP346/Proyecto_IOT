5a7e147f23f4f92a3c974777e9d2191c94a208d7
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2025-05-08 13:42:51 -0500

    Ya no se que más hacer 7u7

--- app/src/main/java/com/example/proyecto_iot/cliente/busqueda/HabitacionAdapter.java
+++ app/src/main/java/com/example/proyecto_iot/cliente/busqueda/HabitacionAdapter.java
@@ -1,5 +1,7 @@
 package com.example.proyecto_iot.cliente.busqueda;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.cliente.pago.PasarellaDePago;
 
 import java.util.List;
 
@@ -36,7 +39,11 @@ public class HabitacionAdapter extends RecyclerView.Adapter<HabitacionAdapter.Vi
         holder.textDescripcion.setText(h.getDescripcion());
 
         holder.btnReservar.setOnClickListener(v -> {
-
+            Context context = v.getContext();
+            Intent intent = new Intent(context, PasarellaDePago.class);
+            intent.putExtra("precio", h.getPrecio());
+            intent.putExtra("descripcion", h.getDescripcion());
+            context.startActivity(intent);
         });
 
         holder.btnVerDetalle.setOnClickListener(v -> {
@@ -51,6 +58,7 @@ public class HabitacionAdapter extends RecyclerView.Adapter<HabitacionAdapter.Vi
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView textPrecio, textEtiqueta, textDescripcion;
         Button btnReservar, btnVerDetalle;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
